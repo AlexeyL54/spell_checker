bc192d5d8d378e24df7820a5a32d2a569523f935
Alexey <alexpushkins@yandex.ru>
2026-03-31 14:03:33 +0300

    realize main methods of vocabulary

--- src/back/vocab.cpp
+++ src/back/vocab.cpp
@@ -1,15 +1,82 @@
 #include "vocab.hpp"
 #include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cwchar>
+#include <exception>
+#include <fstream>
 #include <math.h>
 #include <string>
 #include <vector>
 
+using std::getline;
+using std::ifstream;
 using std::pow;
 using std::vector;
 using std::wstring;
 
+/**
+ * @brief Конструктор
+ *
+ * Инициализировать
+ * поле пути к файлу словаря;
+ * размер алфавита словаря и проверяемого текста
+ */
 Vocabulary::Vocabulary(const string &path)
     : vocab_path(path), alphabet_size(WCHAR_MAX) {}
+
+/**
+ * @brief Вычислить количество строк в файле
+ * @param file ссылка на потокoк
+ * @return количество строк в файле
+ */
+size_t rowsTotal(ifstream &file) {
+  size_t rows = 0;
+  string line;
+
+  while (getline(file, line)) {
+    rows++;
+  }
+  return rows;
+}
+
+/**
+ * @brief Конвертировать string в wstring
+ * @param str ссылка на конвертируемую строку
+ * @return строку типа wstring
+ */
+std::wstring stringToWstring(const std::string &str) {
+  size_t len = str.length();
+  std::wstring wstr(len, L'\0');
+  std::mbstowcs(&wstr[0], str.c_str(), len);
+  return wstr;
+}
+
+/**
+ * @brief Загрузить словарь и представить его в виде хэш-таблицы
+ */
+void Vocabulary::loadVocab() {
+  string line;
+  wstring wline;
+  int wline_hash;
+
+  ifstream file(vocab_path);
+  if (!file.is_open()) {
+    file.close();
+    return;
+  }
+
+  size_t lines_total = Vocabulary::rowsTotal(file);
+  Vocabulary::vocab_hash_table.resize(lines_total);
+
+  while (getline(file, line)) {
+    wline = stringToWstring(line);
+    wline_hash = createHashCode(wline);
+    Vocabulary::vocab_hash_table[wline.length()][wline_hash] = wline;
+  }
+
+  file.close();
+}
 
 /**
  * @brief Создать хэш код для строки
@@ -25,4 +92,21 @@ int Vocabulary::createHashCode(const wstring &str) {
   }
 
   return code;
+}
+
+/**
+ * @brief Проверить наличие строки в словаре по хэш-коду
+ * @param key хэш-код проверяемой строки
+ * @return true, если строка есть в словаре, иначе false
+ */
+bool Vocabulary::isInVocab(int key) {
+  bool found = false;
+
+  try {
+    auto str = Vocabulary::vocab_hash_table.at(key);
+
+  } catch (const std::exception &e) {
+    found = false;
+  }
+  return found;
 }
--- src/back/vocab.hpp
+++ src/back/vocab.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <cstddef>
+#include <fstream>
 #include <memory>
 #include <string>
 #include <sys/types.h>
 #include <unordered_map>
 #include <vector>
 
+using std::ifstream;
 using std::shared_ptr;
 using std::string;
 using std::unordered_map;
@@ -15,18 +18,51 @@ using std::wstring;
 class Vocabulary {
 
 public:
+  /**
+   * @brief Конструктор
+   *
+   * Инициализировать
+   * поле пути к файлу словаря;
+   * размер алфавита словаря и проверяемого текста
+   */
   Vocabulary(const string &path);
 
+  /**
+   * @brief Загрузить словарь и представить его в виде хэш-таблицы
+   */
   void loadVocab();
 
-  wstring readFromFile();
-
+  /**
+   * @brief Создать хэш код для строки
+   * @param str ссылка на строку
+   * @return хэш код
+   */
   int createHashCode(const wstring &str);
 
+  /**
+   * @brief Проверить наличие строки в словаре по хэш-коду
+   * @param key хэш-код проверяемой строки
+   * @return true, если строка есть в словаре, иначе false
+   */
   bool isInVocab(int key);
 
 private:
-  vector<unordered_map<int, wstring>> vocab_hash_table;
-  const string vocab_path;
-  const int alphabet_size;
+  vector<unordered_map<int, wstring>>
+      vocab_hash_table;    // хэш-таблица строк словаря
+  const string vocab_path; // путь к файлу словаря
+  const int alphabet_size; // размер алфавита проверяемого текста и словаря
+
+  /**
+   * @brief Вычислить количество строк в файле
+   * @param file ссылка на потокoк
+   * @return количество строк в файле
+   */
+  size_t rowsTotal(ifstream &file);
+
+  /**
+   * @brief Конвертировать string в wstring
+   * @param str ссылка на конвертируемую строку
+   * @return строку типа wstring
+   */
+  wstring stringToWstring(const string &str);
 };
